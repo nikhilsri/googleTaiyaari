c3ffb1d5356cbdb333ccaadaa39fb0cdcb04ed27
Nikhil Srivastava <nikhil.srivastava944@gmail.com>
2022-05-09 23:27:15 +0530

    Create SpeedTest.java
    Barbara is a speed typer. In order to check her typing speed, she performs a speed test. She is given a string I that she is supposed to type.
    
    While Barbara is typing, she may make some mistakes, such as pressing the wrong key. As her typing speed is important to her, she does not want to spend additional time correcting the mistakes, so she continues to type with the errors until she finishes the speed test. After she finishes the speed test, she produces a P.
    
    Now she wonders how many extra letters she needs to delete in order to get I from P. It is possible that Barbara made a mistake and P cannot be converted back to I just by deleting some letters. In particular, it is possible that Barbara missed some letters.
    
    Help Barbara find out how many extra letters she needs to remove in order to obtain I or if I cannot be obtained from P by removing letters then output IMPOSSIBLE.
    
    Input
    The first line of the input gives the number of test cases, T. T test cases follow.
    
    Each test case has 2 lines. The first line of each test case is an input string I (that denotes the string that the typing test has provided). The next line is the produced string P (that Barbara has entered).
    
    Output
    For each test case, output one line containing Case #x: y, where x is the test case number (starting from 1) and y is the number of extra letters that need to be removed in order to obtain I. If it is not possible to obtain I then output IMPOSSIBLE as y.
    
    Limits
    Memory limit: 1 GB.
    1≤T≤100.
    Both the strings contain letters from a-z and A-Z.
    Length of the given strings will be 1≤|I|,|P|≤105.
    Test Set 1
    Time limit: 20 seconds.
    All letters in I are the same.
    
    Test Set 2
    Time limit: 40 seconds.
    Sample
    Note: there are additional samples that are not run on submissions down below.
    Sample Input
    save_alt
    content_copy
    2
    aaaa
    aaaaa
    bbbbb
    bbbbc
    Sample Output
    save_alt
    content_copy
    Case #1: 1
    Case #2: IMPOSSIBLE
    In the first test case, P contains one extra a, so she needs to remove 1 extra letter in order to obtain I.
    In the second test case, Barbara typed only 4 letters b, while I consists of 5 letters b so the answer is IMPOSSIBLE.
    
    
    Additional Sample - Test Set 2
    The following additional sample fits the limits of Test Set 2. It will not be run against your submitted solutions.
    Sample Input
    save_alt
    content_copy
    2
    Ilovecoding
    IIllovecoding
    KickstartIsFun
    kkickstartiisfun
    Sample Output
    save_alt
    content_copy
    Case #1: 2
    Case #2: IMPOSSIBLE
    In the first test case, P has 2 extra letters, I and l. The other letters are in the order given in I. So she needs to remove 2 letters in order to obtain I.
    In the second test case, there is no letter K in P so the answer is IMPOSSIBLE

---
+++ SpeedTest.java
@@ -0,0 +1,110 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Solution{
+    public static void main(String[] args){
+        
+   InputStream inputStream = System.in;
+
+        InputReader in = new InputReader(inputStream);
+
+        //inanner in = new inanner(System.in);
+        int t = in.nextInt();
+        //System.out.println(t);
+        //in.next();
+        while (t-- > 0) {
+
+            String i = in.next();
+            String p = in.next();
+            ans = 0;
+            if (p.length() < i.length()) {
+                counter++;
+                System.out.println("Case #" + counter + ": " + "IMPOSSIBLE");
+                //return;
+            } else {
+                check(i, p);
+            }
+        }
+    }
+    static int ans = 0;
+    static int counter = 0;
+
+    static void check(String i, String p) {
+        counter++;
+
+        Map<Character, Integer> hmi = new TreeMap<Character, Integer>();
+        Map<Character, Integer> hmp = new TreeMap<Character, Integer>();
+        for (int ii = 0; ii < i.length(); ii++) {
+            if (hmi.containsKey(i.charAt(ii))) {
+                hmi.put(i.charAt(ii), hmi.get(i.charAt(ii)) + 1);
+            } else {
+                hmi.put(i.charAt(ii), 1);
+            }
+        }
+        for (int j = 0; j < p.length(); j++) {
+            if (hmp.containsKey(p.charAt(j))) {
+                hmp.put(p.charAt(j), hmp.get(p.charAt(j)) + 1);
+            } else {
+                hmp.put(p.charAt(j), 1);
+            }
+        }
+        //System.out.println(hmi);
+        //System.out.println(hmp);
+        if (hmp.size() < hmi.size()) {
+            System.out.println("Case #" + counter + ": " + "IMPOSSIBLE");
+            return;
+        }
+
+        for (Map.Entry<Character, Integer> entry : hmp.entrySet()) {
+
+            if (hmi.containsKey(entry.getKey())) {
+
+                if (hmi.get(entry.getKey()) > entry.getValue()) {
+                    System.out.println("Case #" + counter + ": " + "IMPOSSIBLE");
+                    return;
+                } else {
+                    ans += (entry.getValue() - hmi.get(entry.getKey()));
+                }
+            } else {
+                if (hmp.size() == hmi.size()) {
+                    
+                    System.out.println("Case #" + counter + ": " + "IMPOSSIBLE");
+                   
+                    return;
+                }
+                ans += entry.getValue();
+                //System.out.println("Case #"+counter+": "+"IMPOSSIBLE");
+                //return;
+            }
+
+        }
+        System.out.println("Case #" + counter + ": " + ans);
+
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+    
+}
